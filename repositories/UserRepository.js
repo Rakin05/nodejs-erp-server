440d008132da41390844ad99d8ad196c3d257e71
Felix Klotzsche <felixklotzsche@gmail.com>
2013-06-26 11:39:39 +0200

    added q library for nice callback promises

---
+++ repositories/UserRepository.js
@@ -0,0 +1,9 @@
+function UserRepository(){
+    this.createUser = createUser;
+}
+
+function createUser(userName, firstName, lastName, password, email){
+    
+}
+
+module.exports = UserRepository;
--- test/UserRepositoryTest.js
+++ test/UserRepositoryTest.js
@@ -2,18 +2,34 @@ var mongoose = require("mongoose");
 var should = require("should");
 var config = require("../config/test");
 
-
 var UserRepository = require("../repositories/UserRepository");
 
 describe("UserRepository", function(){
+    var repository = null;
     before(function(done){
         // open the connection to mongodb
         mongoose.connect(config.db);
+        repository = new UserRepository();
         done();
     });
 
-    it("should be true that true is true", function(){
-        true.should.equal(true);
+    it("should be true that true is true", function(done){
+        repository.createUser(
+            "Rakin",
+            "Felix", "Klotzsche",
+            "felixklotzsche@gmail.com",
+            "test123").then(function(doc){
+                should.exist(doc);
+                doc.should.have.property("userName");
+                doc.should.have.property("firstName");
+                doc.should.have.property("lastName");
+                doc.should.have.property("email");
+                doc.should.have.property("password");
+                doc.should.have.property("auth_token");
+                done();
+            }).fail(function(err){
+                done(err);
+            });
     });
 
     after(function(done){
